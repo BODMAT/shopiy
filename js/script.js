2ad35f9d1000a91282de59d824ea2ff3da7dbc8c
Bohdan <matula.bohdan@gmail.com>
2024-02-07 23:22:39 +0200

    added block and new js

--- js/script.js
+++ js/script.js
@@ -63,3 +63,94 @@ if (menuLinks.length > 0) {
         }
     }
 }
+
+//!Этот код трешак полный, но написан почти полностью мной и как первый сойдет, может потом оптимизирую =======================
+document.addEventListener("DOMContentLoaded", function () {
+    var textBox = document.querySelectorAll('.suptitle');
+    textBox.forEach(element => {
+        let text = element.innerText;
+        let newHTML = '';
+
+        for (let i = 0; i < text.length; i++) {
+            newHTML += '<span>' + text[i] + '<span>';
+        }
+        element.innerHTML = newHTML;
+    });
+
+    // Получаем нужный элемент
+    var allElements = document.querySelectorAll('.suptitle');
+    allElements.forEach(element => {
+        var Visible = function (target) {
+            // Все позиции элемента
+            var targetPosition = {
+                top: window.pageYOffset + target.getBoundingClientRect().top,
+                left: window.pageXOffset + target.getBoundingClientRect().left,
+                right: window.pageXOffset + target.getBoundingClientRect().right,
+                bottom: window.pageYOffset + target.getBoundingClientRect().bottom
+            },
+                // Получаем позиции окна
+                windowPosition = {
+                    top: window.pageYOffset,
+                    left: window.pageXOffset,
+                    right: window.pageXOffset + document.documentElement.clientWidth,
+                    bottom: window.pageYOffset + document.documentElement.clientHeight
+                };
+
+            if (targetPosition.bottom > windowPosition.top && // Если позиция нижней части элемента больше позиции верхней чайти окна, то элемент виден сверху
+                targetPosition.top < windowPosition.bottom && // Если позиция верхней части элемента меньше позиции нижней чайти окна, то элемент виден снизу
+                targetPosition.right > windowPosition.left && // Если позиция правой стороны элемента больше позиции левой части окна, то элемент виден слева
+                targetPosition.left < windowPosition.right) { // Если позиция левой стороны элемента меньше позиции правой чайти окна, то элемент виден справа
+                // Если элемент полностью видно, то запускаем следующий код
+                element.classList.add("_active-suptitle");
+
+                let spans = element.querySelectorAll('span'),
+                    count = 0,
+                    timeout = 0;
+
+                //TEXT-PRINTING
+                function typing_text() {
+                    spans[count].classList.add('visible');
+                    if (spans[count].innerText == ' ' || spans[count].innerHTML == ' ') {
+                        timeout = Math.floor(Math.random() * Math.floor(100));
+                        //spans[count].classList.add('cursor');
+                    } else {
+                        timeout = 30;
+                        //spans[count].classList.remove('cursor');
+
+                    }
+                    if (count < element.textContent.length * 2 - 1) {
+                        setTimeout(() => {
+                            count++;
+                            typing_text();
+                        }, timeout);
+                    }
+                }
+                const elem = element;
+                document.addEventListener('scroll', onScroll);
+                function onScroll() {
+                    const posTop = elem.getBoundingClientRect().top;
+
+                    if (posTop + elem.clientHeight <= window.innerHeight && posTop >= 0) {
+                        elem.classList.add('visible');
+                        document.removeEventListener('scroll', onScroll);
+
+                        typing_text()
+                    }
+
+                }
+                onScroll()
+                element.removeAttribute("id");
+            } else {
+                // Если элемент не видно, то запускаем этот код
+                element.classList.remove("_active-suptitle");
+
+            };
+        };
+        // Запускаем функцию при прокрутке страницы
+        window.addEventListener('scroll', function () {
+            Visible(element);
+        });
+        // А также запустим функцию сразу. А то вдруг, элемент изначально видно
+        Visible(element);
+    });
+});
