4b4a4bbe1d8efc8ef2579a50e2c983f1748f84db
Bohdan <matula.bohdan@gmail.com>
2024-02-11 22:01:06 +0200

    all blocks done

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 ## SHOPIY
 - Visit [site](https://bodmat.github.io/shopiy/)
+- View [maket](https://www.figma.com/file/MmWflfWvsjHLMCmy7PoQl2/eCommercePlatformMarketingAgencyLandingPage)
 - Use HTML, CSS (SCSS), JS
--- js/script.js
+++ js/script.js
@@ -30,7 +30,8 @@ if (iconMenu) {
 }
 
 // При скролле
-const menuLinks = document.querySelectorAll('.menu__link[data-goto]');
+const menuLinks = document.querySelectorAll('.menu__link[data-goto],.menu2__link[data-goto]');
+console.log(menuLinks);
 if (menuLinks.length > 0) {
     menuLinks.forEach(menuLink => {
         menuLink.addEventListener("click", onMenuLinkClick);
@@ -155,3 +156,42 @@ document.addEventListener("DOMContentLoaded", function () {
     });
 });
 
+//!============================================================================
+// Получаем нужный элемент
+var process = document.querySelector('.absolute-process');
+
+var Visible = function (target) {
+    // Все позиции элемента
+    var targetPosition = {
+        top: window.pageYOffset + target.getBoundingClientRect().top,
+        left: window.pageXOffset + target.getBoundingClientRect().left,
+        right: window.pageXOffset + target.getBoundingClientRect().right,
+        bottom: window.pageYOffset + target.getBoundingClientRect().bottom
+    },
+        // Получаем позиции окна
+        windowPosition = {
+            top: window.pageYOffset,
+            left: window.pageXOffset,
+            right: window.pageXOffset + document.documentElement.clientWidth,
+            bottom: window.pageYOffset + document.documentElement.clientHeight
+        };
+
+    if (targetPosition.bottom > windowPosition.top && // Если позиция нижней части элемента больше позиции верхней чайти окна, то элемент виден сверху
+        targetPosition.top < windowPosition.bottom && // Если позиция верхней части элемента меньше позиции нижней чайти окна, то элемент виден снизу
+        targetPosition.right > windowPosition.left && // Если позиция правой стороны элемента больше позиции левой части окна, то элемент виден слева
+        targetPosition.left < windowPosition.right) { // Если позиция левой стороны элемента меньше позиции правой чайти окна, то элемент виден справа
+        // Если элемент полностью видно, то запускаем следующий код
+        process.classList.add("_active");
+    } else {
+        // Если элемент не видно, то запускаем этот код
+        process.classList.remove("_active");
+    };
+};
+
+// Запускаем функцию при прокрутке страницы
+window.addEventListener('scroll', function () {
+    Visible(process);
+});
+
+// А также запустим функцию сразу. А то вдруг, элемент изначально видно
+Visible(process);
