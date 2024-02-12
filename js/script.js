7dc997335a17d600b39e854ef25aa42c77ec3d6c
Bohdan <matula.bohdan@gmail.com>
2024-02-12 22:46:44 +0200

    added pop-ups and animation

--- js/script.js
+++ js/script.js
@@ -195,3 +195,49 @@ window.addEventListener('scroll', function () {
 
 // А также запустим функцию сразу. А то вдруг, элемент изначально видно
 Visible(process);
+
+
+//!=============
+const input = document.querySelector('#clientInputCardnum');
+
+/** Валидация цифр при вводе*/
+input.addEventListener('keypress', function (evt) {
+    if (evt.keyCode < 48 || evt.keyCode > 57) evt.preventDefault();
+});
+
+
+/** Оформление вывода по шаблону */
+input.addEventListener('input', function (evt) {
+    const pattern = '**** **** **** ****';
+    let value = input.value;
+    value = getCleanValue(value);
+    value = getPatternedValue(value, pattern);
+    input.value = value;
+});
+
+/** Возвращает очищенную строку (только цифры)*/
+function getCleanValue(string) {
+    let newString = '';
+    for (let i = 0; i < string.length; i++) {
+        if (+string[i]) newString += string[i];
+    }
+    return newString;
+}
+
+/** Возвращает оформленную по шаблону строку*/
+function getPatternedValue(string, pattern) {
+    let newString = '';
+    let counter = 0;
+
+    for (let i = 0; i < pattern.length; i++) {
+        if (!string[counter]) continue;
+        if (pattern[i] === '*') {
+            newString += string[counter];
+            counter++;
+            continue;
+        }
+        newString += pattern[i];
+    }
+
+    return newString;
+}
